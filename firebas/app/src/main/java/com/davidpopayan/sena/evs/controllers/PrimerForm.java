48d73b85c6880a634f0946dbeee5d8e3bd0fd45e
DavidPyn27 <popayan159@gmail.com>
2018-09-19 12:59:27 -0500

    formulario personal

--- firebas/app/src/main/java/com/davidpopayan/sena/evs/controllers/PrimerForm.java
+++ firebas/app/src/main/java/com/davidpopayan/sena/evs/controllers/PrimerForm.java
@@ -2,6 +2,7 @@ package com.davidpopayan.sena.evs.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.davidpopayan.sena.evs.R;
@@ -11,7 +12,7 @@ import java.util.List;
 
 public class PrimerForm extends AppCompatActivity {
 
-    Spinner spinnerEps;
+    Spinner spinnerGenero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +20,22 @@ public class PrimerForm extends AppCompatActivity {
         setContentView(R.layout.activity_primer_form);
 
         inicializar();
+        listarGenero();
 
+    }
+
+    private void listarGenero() {
+        List<String>genero = new ArrayList<>();
+        genero.add("Masculino");
+        genero.add("Femenino");
+        genero.add("Indefinido");
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, genero);
+        spinnerGenero.setAdapter(adapter);
     }
 
 
     private void inicializar() {
+        spinnerGenero = findViewById(R.id.spinnerGenero);
     }
 }
